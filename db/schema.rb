615d11ab89be963654ecb6d7e81b708c4c9f4429
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-05-24 18:03:39 +0900

    Completed db migration from studio model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_080437) do
+ActiveRecord::Schema.define(version: 2021_05_24_085218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "rate"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_studios_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 2021_05_24_080437) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "studios", "users"
 end
