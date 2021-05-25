6b8647acd2c41cfff0d1b6f1f2b03754806423df
Lena Namiki <lena.namiki@gmail.com>
2021-05-25 11:21:17 +0900

    Added validations to booking, studio and user model

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,6 @@
 class Studio < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :rate, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
   has_many :bookings
   has_many :studios
   has_many :bookings_as_owner, through: :studios, source: :bookings
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true
 end
