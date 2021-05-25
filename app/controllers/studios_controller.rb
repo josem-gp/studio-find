955ec04e206f49e35e32c1355a28cc68f62d1eba
Lena Namiki <lena.namiki@gmail.com>
2021-05-25 15:37:41 +0900

    Changed setting to skip authentication for studios#index and studios#show

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,4 +1,5 @@
 class StudiosController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @studios = policy_scope(Studio)
     authorize @studios
