3b39c3ffd59f9e1cbdfc0032ee4f9ba37fc046ad
Lena Namiki <lena.namiki@gmail.com>
2021-05-25 13:09:37 +0900

    Assigned new booking to user and studio

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,13 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.studio = params[:id]
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
   end
 
   private
