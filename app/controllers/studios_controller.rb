6bd91179838ff2b75b6e0fb12a17b8f3ae4d6959
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-05-25 15:55:19 +0900

    Added css styles to the form

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -2,10 +2,7 @@ class StudiosController < ApplicationController
   def index
     @studios = Studio.all
   end
-<<<<<<< HEAD
     
-=======
->>>>>>> master
   def show
     @studio = Studio.find(params[:id])
     @booking = Booking.new
