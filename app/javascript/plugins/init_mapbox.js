5fb6cdcafe4772d6cec5c3e5d9254a59ed89972f
Lena Namiki <lena.namiki@gmail.com>
2021-05-26 14:19:38 +0900

    Info box showing

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -19,12 +19,17 @@ const initMapbox = () => {
     });
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
     });
     fitMapToMarkers(map, markers);
   }
+
+
 };
 
 export { initMapbox };
