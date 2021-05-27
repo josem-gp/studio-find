5f9c9cfd8a194b46a8e77330a3e1d6c7e7dc4790
Lena Namiki <lena.namiki@gmail.com>
2021-05-27 12:45:49 +0900

    owners bookings#index created

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,13 +1,14 @@
-class BookingsController < ApplicationController
+class Owner::BookingsController < ApplicationController
+  def index
+    @bookings = authorize [:owner, current_user.bookings]
+    policy_scope(@bookings)
+  end
 
   def edit
-    if current_user.studio
-
-    end
+    @booking = authorize [:owner, Booking.find(params[:id])]
   end
 
   def update
 
   end
-
 end
