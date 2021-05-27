c3c210f842db6cb085fc40c7ae9032c7e08d8b9b
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-05-27 12:11:12 +0900

    Added dependent destroy to bookings & studios

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :bookings
-  has_many :studios
+  has_many :bookings, dependent: :destroy
+  has_many :studios, dependent: :destroy
   has_many :bookings_as_owner, through: :studios, source: :bookings
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true, length: { minimum: 6 }
