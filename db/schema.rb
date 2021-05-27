307267a38cfea859755110f01df8c853f5aa8f0c
Kenn Vun Gan <gervasiusgan@gmail.com>
2021-05-27 15:58:24 +0900

    Added area capacity description to studio migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_020402) do
+ActiveRecord::Schema.define(version: 2021_05_27_065351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,9 @@ ActiveRecord::Schema.define(version: 2021_05_26_020402) do
     t.string "images"
     t.float "latitude"
     t.float "longitude"
+    t.integer "area"
+    t.integer "capacity"
+    t.text "description"
     t.index ["user_id"], name: "index_studios_on_user_id"
   end
 
