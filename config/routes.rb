ef4270cc8ae2cedc72cf99f1b36351455d2e2d44
Lena Namiki <lena.namiki@gmail.com>
2021-05-27 10:53:56 +0900

    Updated routes with owner

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :studios, only: [:index, :show] do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :edit, :update, :destroy]
   end
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :edit, :update]
+  namespace :owner do
+    resources :studios, except: [:show]
+    resources :bookings, only: [:edit, :update]
+  end
 end
