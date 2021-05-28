bf208ddf605b1e11c32602f2e2b08e798a13b6d7
Lena Namiki <lena.namiki@gmail.com>
2021-05-28 11:44:15 +0900

    Changed status naming to pending, confirmed, declined

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :studio
   validates :start_date, presence: true
   validates :end_date, presence: true
-  enum status: [ :pending, :confirmed, :denied ]
+  enum status: [ :pending, :confirmed, :declined ]
 end
